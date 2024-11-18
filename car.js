89babe788785f99ff3e592b2545f719583e54e54
rkSha-git <rimshashoukat271@gmail.com>
2024-11-18 19:26:57 +0800

    finalized display page

--- car.js
+++ car.js
@@ -12,3 +12,20 @@ function slides(){
     slide[slideIndex-1].style.display = 'block';
     setTimeout(slides, 5000);
 }
+
+let nav = document.getElementsByClassName('nav')[0];
+let navBtn = document.getElementsByClassName('navBtn')[0];
+
+let show = false;
+
+navBtn.addEventListener('click',()=>{
+    if(show){
+        show = false;
+        navBtn.innerHTML = `<i class="fas fa-bars fa-2xl" style="line-height: 1; color: #dedede;"></i>`
+        nav.style.display = "none";
+    }else{
+        show = true;
+        navBtn.innerHTML = `<i class="fa-solid fa-xmark fa-2xl" style="color: #dedede;"></i>`;
+        nav.style.display = "block";
+    }
+});
