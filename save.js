a12e5aeb46e3924678c70d2cc8c75530a4ec46a3
rkSha-git <rimshashoukat271@gmail.com>
2024-12-10 19:03:35 +0800

    making save.js responsive

--- save.js
+++ save.js
@@ -8,9 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
         let saveCars = cars.filter(car => car.status === 'true');
         saveCars.forEach((car) => {
             cartHtml += `
-            <div class="bg-gray-300 w-1/2 border-2 border-black rounded-md flex flex-row shadow-md shadow-gray-300 mb-4">
-                <img class="w-2/4 rounded-md" src="${car.image}" alt="${car.name}">
-                <div class="w-2/4 p-4">
+            <div class="bg-gray-300 max-lg:w-full max-sm:flex-col w-1/2 border-2 border-black rounded-md flex flex-row shadow-md shadow-gray-300 mb-4">
+                <img class="w-2/4 max-sm:w-screen rounded-md" src="${car.image}" alt="${car.name}">
+                <div class="w-2/4 max-sm:w-screen p-4">
                     <h2 class="text-gray-900 font-bold text-2xl mb-2">${car.name} ${car.color}</h2>
                     <h2 class="text-gray-800 text-xl mb-2">${car.model} ${car.year}</h2>
                     <p class="text-gray-500 text-sm text-wrap">${car.description}</p>
